2874389f86b9159ef2618083c5bedf09a54a0bbe
Caalb <caalb.craft@gmail.com>
2022-11-07 11:31:35 -0300

    project 01

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerLife: 100,
+        mosterLife: 100,
+        iniciarGame: false,
+        logAtaque: [],
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    watch: {
+
+    }
+})
