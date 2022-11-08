7fedc77d9ba83784f3cae7ff83fa5003ef5741a2
Caalb <caalb.craft@gmail.com>
2022-11-08 11:16:17 -0300

    initializing with component

---
+++ Componentes/componente-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="app">
+  <button @click="contador++">Add 1</button>
+  <h1>Contadores</h1>
+  {{contador}}
+ </div>
+</template>
+
+<script>
+export default ({
+  data() {
+    return {
+      contador:0
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
