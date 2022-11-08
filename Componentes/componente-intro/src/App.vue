44f43371794a8c9ccc5a6cd9a4ee23a4cc01fcfc
Caalb <caalb.craft@gmail.com>
2022-11-08 11:57:04 -0300

    css with scope

--- Componentes/componente-intro/src/App.vue
+++ Componentes/componente-intro/src/App.vue
@@ -1,21 +1,15 @@
 <template>
  <div id="app">
-  <button @click="contador++">Add 1</button>
-  <h1>Contadores</h1>
-  {{contador}}
+  <app-contadores />
  </div>
 </template>
 
 <script>
 export default ({
-  data() {
-    return {
-      contador:0
-    }
-  }
+
 })
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- Componentes/componente-intro/src/main.js
+++ Componentes/componente-intro/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ContadoresVue from './ContadoresVue.vue'
 
 Vue.config.productionTip = false
+Vue.component('app-contadores', ContadoresVue)
 
 new Vue({
   render: h => h(App),
