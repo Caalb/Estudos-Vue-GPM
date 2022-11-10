4060cfeb65665d4b86d10b3a51233593297e7ba8
Caalb <caalb.craft@gmail.com>
2022-11-10 10:20:44 -0300

    communication dad-son

--- Documentacao-Vue/components/src/App.vue
+++ Documentacao-Vue/components/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld v-for="post in posts" v-bind:key="post.id" v-bind:post="post"></HelloWorld>
+    <div :style="{ fontSize: postFontSize + 'em'}">
+      <HelloWorld v-for="post in posts" v-bind:key="post.id" v-bind:post="post" v-on:enlarge-text="postFontSize += $event" v-on:decrease-text="postFontSize -= $event"></HelloWorld>
+    </div>
   </div>
 </template>
 
@@ -15,11 +16,12 @@ export default {
   },
   data() {
     return {
+      postFontSize: 1,
       posts: [
         { id: 1, title: 'Minha jornada com Vue' },
         { id: 2, title: 'Postagens com Vue' },
-        { id: 3, title: 'Porque Vue é tão divertido' },
-        { id: 4, title: "Teste" }
+        { id: 3, title: 'Porque Vue é tão divertido', content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolore alias consequatur porro! Sint autem culpa dolorum corrupti. Sit harum maxime veritatis ipsa soluta iste est saepe fugiat placeat vero." },
+        { id: 4, title: "Teste", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolore alias consequatur porro! Sint autem culpa dolorum corrupti. Sit harum maxime veritatis ipsa soluta iste est saepe fugiat placeat vero."}
       ]
     }
   }
--- Documentacao-Vue/components/src/components/HelloWorld.vue
+++ Documentacao-Vue/components/src/components/HelloWorld.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h3>{{post.title}}</h3>
-    <h6>ID: {{post.id}}</h6>
+    <button @click="$emit('enlarge-text', 0.1)">Aumentar Fonte</button>
+    <button @click="$emit('decrease-text', 0.1)">Diminuir Fonte</button>
+    <p>Conteudo: {{post.content}}</p>
   </div>
 </template>
 <script>
 
 export default {
-  props: ['post'],
+  props: {
+    post: Object,
+  },
 }
 </script>
 
