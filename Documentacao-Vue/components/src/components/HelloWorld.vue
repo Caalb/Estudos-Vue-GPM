af2a1e04fdd3b8aa51a81a6d06eef2a136386ae4
Caalb <caalb.craft@gmail.com>
2022-11-10 09:14:01 -0300

    count++

---
+++ Documentacao-Vue/components/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button">
+    {{count}}
+    <button @click="aumentar">Aumentar 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    aumentar() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
