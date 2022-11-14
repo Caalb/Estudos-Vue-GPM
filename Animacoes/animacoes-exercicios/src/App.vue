80ccedfad9cca4c34e611fc46a2be75bc85feb72
Caalb <caalb.craft@gmail.com>
2022-11-14 16:07:39 -0300

    fade/slide

--- Animacoes/animacoes-exercicios/src/App.vue
+++ Animacoes/animacoes-exercicios/src/App.vue
@@ -1,13 +1,27 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar mensagem</b-button>
+		<transition name="fade">
+			<b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
+
+		<transition name="slide">
+			<b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+	data() {
+		return {
+			msg: 'Uma mensagem de informação para o usuário!',
+			exibir: false,
+		}
+	}
 }
 </script>
 
@@ -19,6 +33,32 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
-	font-size: 1.5rem;
+	font-size: 1rem;
+}
+
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active  {
+	transition: opacity 1s;
+}
+
+@keyframes slide-in {
+	from {  transform: translateY(40px) }
+	to { transform: translateY(0)}
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0);}
+	to {transform: translateY(40px);}
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+}
+
+.slide-leave-active {
+	animation: slide-out 2s ease;
 }
 </style>
