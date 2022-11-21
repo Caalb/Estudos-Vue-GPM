6f377a71164b568f0fa149977770015df8bc8dd0
Caalb <caalb.craft@gmail.com>
2022-11-21 12:25:05 -0300

    vuex practicing

---
+++ Praticando-Vuex/praticando-vuex/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <p>{{$store.state.user.first_name}}</p>
+    <input type="text" v-model="myName">
+    <button @click="saveName">Save</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      myName: ''
+    }
+  },
+  methods: {
+    saveName() {
+      // Action
+      this.$store.dispatch('saveFistName', this.myName)
+
+
+      // Muttation
+      // this.$store.commit()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Praticando-Vuex/praticando-vuex/src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    user: {
+      first_name: 'Jon',
+      last_name: 'Snow',
+      email: 'jon@snow.com'
+    }
+  },
+  getters: {
+  },
+  mutations: {
+    saveFirstName(state, payload) {
+      state.user.first_name = payload
+    }
+  },
+  actions: {
+    saveFistName(context, payload){
+      context.commit('saveFirstName', payload)
+    }
+  },
+  modules: {
+  }
+})
