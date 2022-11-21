37c6be5a17c872109530f64f85a69b77fa6eced7
Caalb <caalb.craft@gmail.com>
2022-11-21 13:58:14 -0300

    vuex v-model

--- Praticando-Vuex/praticando-vuex/src/App.vue
+++ Praticando-Vuex/praticando-vuex/src/App.vue
@@ -1,45 +1,45 @@
 <template>
   <div id="app">
-    <p>{{$store.state.user.first_name}}</p>
-    <input type="text" v-model="myName">
-    <button @click="saveName">Save</button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container mt-10">
+      <p>{{ $store.state.user.first_name }}</p> <br>
+      <label for="">Primeiro nome</label>
+      <input type="text" v-model="firstName" class="form-control">
+      <button @click="saveName" class="btn btn-primary">Save</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data() {
     return {
       myName: ''
     }
   },
+  computed: {
+    firstName: {
+      get() {
+        return this.$store.state.user.first_name
+      },
+      set(value) {
+        this.$store.commit('saveFirstName', value)
+      }
+    }
+  },
   methods: {
     saveName() {
       // Action
-      this.$store.dispatch('saveFistName', this.myName)
-
+      // this.$store.dispatch('saveFistName', this.myName)
 
       // Muttation
-      // this.$store.commit()
+      this.$store.commit('saveFirstName', this.myName)
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
