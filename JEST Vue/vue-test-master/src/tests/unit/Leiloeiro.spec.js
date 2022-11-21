07af99c9bf0d2588421ca929a996572ff3233b42
Caalb <caalb.craft@gmail.com>
2022-11-21 10:42:10 -0300

    Jest

---
+++ JEST Vue/vue-test-master/src/tests/unit/Leiloeiro.spec.js
@@ -0,0 +1,51 @@
+import Leiloeiro from '../../views/Leiloeiro'
+import { mount } from '@vue/test-utils'
+import { getLeilao, getLances } from '@/http'
+import flushPromises from 'flush-promises'
+
+jest.mock('@/http')
+const leilao = {
+  produto: 'Livro na casa do código',
+  lanceInicial: 50,
+  descricao: 'Livro bem bacana sobre Vue'
+}
+
+describe('Leiloeiro inicia um leilão que não possui lances', () => {
+  test('avisa quando não existem lances', async () => {
+
+    getLeilao.mockResolvedValueOnce(leilao)
+    getLances.mockResolvedValueOnce([])
+
+    const wrapper = mount(Leiloeiro, {
+      propsData: {
+        id: 1
+      }
+    })
+
+    await flushPromises()
+
+    const alerta = wrapper.find('.alert-dark')
+    expect(alerta.exists()).toBe(true)
+
+  })
+})
+
+describe('Um leiloeiro exibe os lances existentes', () => {
+  test('Não mostra o aviso de "sem lances"', async () => {
+
+  })
+
+  test('Possui uma lista de lances', async () => {
+
+  })
+})
+
+describe('Um leiloeiro comunica os valores de menor e maior lance', () => {
+  test('Mostra o maior lance daquele leilão', async () => {
+
+  })
+
+  test('Mostra o menor lance daquele leilão', async () => {
+
+  })
+})
